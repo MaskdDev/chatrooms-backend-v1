223d57844a0d0cd5a3d18be70893d40a029bda9a
MaskdDev <maskddev@gmail.com>
2026-02-05 18:05:07 +1100

    Created room_members and room_invites tables

---
+++ migrations/2_room_members.sql
@@ -0,0 +1,29 @@
+create table if not exists "room_members"
+(
+    -- Identification
+    member_id  bigint      not null,
+    room_id    bigint      not null,
+
+    -- Record keeping
+    created_at timestamptz not null default now(),
+    updated_at timestamptz not null default now(),
+
+    -- Constraints
+    primary key (member_id, room_id),
+
+    -- Foreign keys
+    constraint fkey_room_members_member_id foreign key (member_id)
+        references "user" (id)
+        on delete cascade,
+    constraint fkey_room_members_room_id foreign key (room_id)
+        references "rooms" (room_id)
+        on delete cascade
+);
+
+create index if not exists index_room_members_room_id on "room_members" (room_id);
+
+create trigger room_members_set_updated_at
+    before update
+    on room_members
+    for each row
+execute function set_updated_at();
---
+++ migrations/3_room_invites.sql
@@ -0,0 +1,30 @@
+create table if not exists "room_invites"
+(
+    -- Identification
+    invite_code text primary key,
+    room_id     bigint      not null,
+    creator_id  text        not null,
+
+    -- Invite information
+    uses        int         not null default 0,
+    max_uses    int                  default null,
+    expires     timestamptz          default null,
+
+    -- Record keeping
+    created_at  timestamptz not null default now(),
+    updated_at  timestamptz not null default now(),
+
+    -- Foreign keys
+    constraint fkey_room_invites_room_id foreign key (room_id)
+        references "rooms" (room_id)
+        on delete cascade,
+    constraint fkey_room_invites_creator_id foreign key (creator_id)
+        references "user" (id)
+        on delete cascade
+);
+
+create trigger room_invites_set_updated_at
+    before update
+    on room_invites
+    for each row
+execute function set_updated_at();
