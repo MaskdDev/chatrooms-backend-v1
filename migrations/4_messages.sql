5f35c5ebd153e770d326c02faa6f8a5349b1ecde
MaskdDev <maskddev@gmail.com>
2026-02-05 18:12:53 +1100

    Created messages table

---
+++ migrations/4_messages.sql
@@ -0,0 +1,31 @@
+create table if not exists "messages"
+(
+    -- Identification
+    message_id     bigint primary key,
+    room_id        bigint      not null,
+    author_id      text        not null,
+
+    -- Message information
+    content        text        not null,
+    timestamp      timestamptz not null,
+    edit_timestamp timestamptz          default null,
+
+    -- Record keeping
+    created_at     timestamptz not null default now(),
+    updated_at     timestamptz not null default now(),
+
+    -- Foreign keys
+    constraint fkey_messages_room_id foreign key (room_id)
+        references "rooms" (room_id)
+        on delete cascade,
+    constraint fkey_messages_author_id foreign key (author_id)
+        references "user" (id)
+);
+
+create index if not exists index_messages_room_id on "messages" (room_id);
+
+create trigger messages_set_updated_at
+    before update
+    on "messages"
+    for each row
+execute function set_updated_at();
