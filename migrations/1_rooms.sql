245031860b7e73ac0f74e0e6ff6772bc9716d3b6
MaskdDev <maskddev@gmail.com>
2026-02-05 17:42:22 +1100

    Created rooms table in database

---
+++ migrations/1_rooms.sql
@@ -0,0 +1,20 @@
+create table if not exists "rooms"
+(
+    -- Identification
+    room_id     bigint primary key,
+
+    -- Room information
+    name        text        not null,
+    description text,
+    visibility  boolean     not null default false,
+
+    -- Record keeping
+    created_at  timestamptz not null default now(),
+    updated_at  timestamptz not null default now()
+);
+
+create trigger rooms_set_updated_at
+    before update
+    on rooms
+    for each row
+execute function set_updated_at();
