f60aec2a394b74a2a386d3b5b9aa8ac9df494aab
MaskdDev <maskddev@gmail.com>
2026-02-06 17:35:03 +1100

    Added GET /rooms/:roomId route and checks for POST /rooms

--- routes/rooms.ts
+++ routes/rooms.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { requireAuth } from "../utils/middleware.ts";
-import { createRoom, getUserRooms } from "../queries/rooms.ts";
+import { createRoom, getRoom, getUserRooms } from "../queries/rooms.ts";
 import type { RoomCreate } from "../utils/types.ts";
 
 // Create router for route group
@@ -45,6 +45,21 @@ router.post("/", async (req, res) => {
       body.description = null;
     }
 
+    // Check for length issues
+    if (body.name.length > 20 || body.name.length < 3) {
+      return res.status(422).json({
+        code: 422,
+        message: "Room name must be between 3 and 20 characters.",
+      });
+    }
+
+    if (body.description && body.description.length > 150) {
+      return res.status(422).json({
+        code: 422,
+        message: "Room description cannot exceed 150 characters.",
+      });
+    }
+
     // Create room
     const room = await createRoom(user.id, body.name, body.description);
 
@@ -52,6 +67,26 @@ router.post("/", async (req, res) => {
     res.status(201).json(room);
   } else {
     res.sendStatus(401);
+  }
+});
+
+/**
+ * Get information on a specific room
+ */
+router.get("/:roomId", async (req, res) => {
+  // Get room ID
+  const roomId = BigInt(req.params.roomId);
+
+  // Get room
+  const room = await getRoom(roomId);
+
+  // Check if room exists
+  if (room) {
+    res.status(200).json(room);
+  } else {
+    res
+      .status(404)
+      .json({ code: 404, message: "Room with given ID not found." });
   }
 });
 
